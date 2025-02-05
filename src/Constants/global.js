837005b086bf59d19c233302876998f0514bebf2
Sejator <sejatordev@gmail.com>
2025-02-05 18:17:13 +0700

    update jam

--- src/Constants/global.js
+++ src/Constants/global.js
@@ -9,7 +9,7 @@ const detail_pasangan = {
     ortu: "Bapak Sungkono (alm) & Ibu Robiah",
   },
   wanita: {
-    nama: "Dwi Siti Lndah Lestari",
+    nama: "Dwi Siti Indah Lestari",
     ortu: "Bapak Sutejo & Ibu Jakiyah",
   },
 };
@@ -26,7 +26,7 @@ const detail_acara = {
   resepsi: {
     hari: "MINGGU",
     tanggal: "16 FEBRUARI 2025",
-    waktu: "PUKUL 10.00 WIB - SELESAI",
+    waktu: "PUKUL 10.30 WIB - SELESAI",
     tempat: "Jln. Marditomo RT 02/09 Sidorejo Kidul, Kec. Tingkir, Kota Salatiga",
     maps_frame:
       "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3957.1338799675896!2d110.5203252!3d-7.3388586!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a79f75fc59dd3%3A0xa4f5885b277a4297!2sTambal%20Ban%20Mbah%20Jo!5e0!3m2!1sen!2sid!4v1737898107589!5m2!1sen!2sid",
